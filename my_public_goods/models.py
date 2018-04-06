c04aa7b458afa69b25ad0909a9ab1e9dadbf5c34
Daniel Kostic <danielkeske@hotmail.com>
2018-04-06 16:35:22 +0200

    Initial commit with all the files. Includes all games

---
+++ my_public_goods/models.py
@@ -0,0 +1,33 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'my_public_goods'
+    players_per_group = 3
+    endowment = c(100)
+    multiplier = 2
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
+
+
+class Player(BasePlayer):
+    contribution = models.CurrencyField(min=0, max=Constants.endowment)
